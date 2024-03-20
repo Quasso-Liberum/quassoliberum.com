b884993d6d0880c378900c6193077d29f160ebc1
SharoonRafeek <sharoonrafeek@gmail.com>
2024-03-20 21:42:28 +0530

    Update page styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="w-screen h-screen bg-[#000] justify-center flex items-center text-[#ddd]">
-      <p className="text-5xl h-max bg-gradient-to-r from-[#d97706] to-[#451a03] via-[#92400e] bg-clip-text text-transparent">
+    <main className="w-screen h-screen bg-[#ddd] justify-center flex items-center">
+      <p className="text-7xl pb-2 bg-gradient-to-r from-[#7E1169] to-[#FDC501] via-[#DC2133] bg-clip-text text-transparent">
         Coming Soon
       </p>
     </main>
