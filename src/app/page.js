c2cc5d83a2675faed453ac253c6672fb8eb42660
Adhil Roshan <58517896+adhilroshan@users.noreply.github.com>
2024-03-20 17:46:15 +0530

    Update font size in Home component

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="w-screen h-screen bg-[#000] justify-center flex items-center text-[#ddd]">
-      <p className="text-9xl">
+      <p className="text-5xl">
       Coming Soon
       </p>
     </main>
