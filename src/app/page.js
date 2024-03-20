2b54721cbe930afd32b53432081b78812081afa0
Adhil Roshan <58517896+adhilroshan@users.noreply.github.com>
2024-03-20 17:42:44 +0530

    Add @next/font dependency and update layout.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="w-screen h-screen bg-[#000] justify-center flex items-center text-[#ddd]">
+      <p className="text-9xl">
       Coming Soon
+      </p>
     </main>
   );
 }
