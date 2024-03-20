b78e1602fb74dc011e984010b368d4c83efd16bc
SharoonRafeek <sharoonrafeek@gmail.com>
2024-03-20 20:27:50 +0530

    Update README.md and page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
-
 export default function Home() {
   return (
     <main className="w-screen h-screen bg-[#000] justify-center flex items-center text-[#ddd]">
-      <p className="text-5xl">
-      Coming Soon
+      <p className="text-5xl h-max bg-gradient-to-r from-[#d97706] to-[#451a03] via-[#92400e] bg-clip-text text-transparent">
+        Coming Soon
       </p>
     </main>
   );
