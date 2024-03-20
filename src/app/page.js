7cad9a30eef30f03a1093ac154db233d99df419c
SharoonRafeek <sharoonrafeek@gmail.com>
2024-03-20 21:48:02 +0530

    Update font size in Home component

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="w-screen h-screen bg-[#ddd] justify-center flex items-center">
-      <p className="text-7xl pb-2 bg-gradient-to-r from-[#7E1169] to-[#FDC501] via-[#DC2133] bg-clip-text text-transparent">
+      <p className="text-5xl md:text-5xl lg:text-7xl pb-2 bg-gradient-to-r from-[#7E1169] to-[#FDC501] via-[#DC2133] bg-clip-text text-transparent">
         Coming Soon
       </p>
     </main>
